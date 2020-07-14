94a03b7d4420cf68dc92edf8edebb5df1e48601f
lindsayTrusted <59939546+lindsayTrusted@users.noreply.github.com>
2020-07-14 13:44:42 -0700

    Heroku Deployed

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const http = require('http');
+const express = require('express');
+const app = new express();
+const http = require('http').Server(app);
+
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get('/', function(request, response){
+	response.statusCode = 200;
+    response.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port,() => {
+//const server = http.createServer((req, res) => {
+ // res.statusCode = 200;
+ // res.sendFile(__dirname + '/index.html');
+  //res.setHeader('Content-Type', 'text/html');
+  //res.end('<h1>Hello World</h1>');
+//});
+
+app.listen(port,() => {
   console.log(`Server running at port `+port);
 });
+
