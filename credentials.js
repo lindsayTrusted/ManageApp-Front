42b0c94db792326a0e85d90e17bf9c724aef5e06
lindsayTrusted <59939546+lindsayTrusted@users.noreply.github.com>
2020-07-14 01:27:31 -0700

    test13

--- credentials.js
+++ credentials.js
@@ -11,3 +11,26 @@ function mockQueryCRM(email) {
 function mockPostNote() {
   return "fake note"
 }
+
+
+function visitCredentials(TUUID){
+  base_url = "https://app.trustedhealth.com/credentials?sfdc_user_id=0053m00000AvSIBAA3&user_uuid="
+  manageApp_url = base_url+TUUID
+  window.location.replace(manageApp_url);
+  return
+}
+
+function getContact(FrontID){
+  var data = null;
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+    }
+  });
+
+  xhr.open("GET", "https://api2.frontapp.com/contacts/"+FrontID);
+  xhr.send(data);
+  return data
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let globalContext;
+let globalUser;
 const assignButton = document.getElementById('assign');
 
 Front.contextUpdates.subscribe(context => {
@@ -6,13 +7,14 @@ Front.contextUpdates.subscribe(context => {
 
   globalContext = context;
 
+  globalUser = context.conversation.recipient.contact;
+
   var displayTeammate = document.getElementById('frontTeammate');
   displayTeammate.innerHTML = 'Hello ' + context.teammate.name.split(' ')[0] + ' 👋';
 
   var messageRecipient = document.getElementById('messageRecipient');
-  messageRecipient.innerHTML = 'Displaying ' + context.conversation.recipient.contact.name;
-
-  console.log('Contact:', context.conversation.recipient.contact);
+  messageRecipient.innerHTML = 'Displaying ' + globalUser.name;
+  console.log('Contact:', globalUser);
 
   assignButton.removeEventListener('click', _assign);
 
@@ -46,88 +48,13 @@ Front.contextUpdates.subscribe(context => {
   }
 });
 
-
 function assign(context) {
   console.log('context in assign() method');
   console.log(context);
   Front.assign(context.teammate.id);
 }
 
-function unassign() {
-  Front.assign(null);
-}
-
-function insertBasicDraft() {
-  Front.createDraft({
-        content: {
-            body: 'Here\'s a draft!',
-            type: 'text'
-        },
-        replyOptions: {
-            type: 'reply',
-            originalMessageId: 'msg_bnmrao3'
-        }
-    });
-}
-
-async function insertDraftReply() {
-  console.log('called insertDraftReply');
-  let messageId = await getMessage();
-  let draft = await Front.createDraft({
-      content: {
-          body: 'Here\'s a draft!',
-          type: 'text'
-      },
-      replyOptions: {
-          type: 'reply',
-          originalMessageId: messageId
-      }
-  });
-  console.log('Draft Created: ', draft);
-}
-
-async function listTags() {
-  let tags = await Front.listTags();
-  console.log('List of Tags: ', tags);
-}
-
-function openUrl() {
-  Front.openUrl('https://frontapp.com');
-}
-
-function openUrlInPopup() {
-  Front.openUrlInPopup('https://suspicious-wozniak-9e3a7e.netlify.com/popup.html');
-}
-
-async function peerReviewDraft() {
-  let messages = await Front.listMessages();
-
-  console.log('Fetched messages: ', messages);
-
-  let message = messages.results[messages.results.length - 1];
-
-  Front.createDraft({
-    cc: ['kenji@frontapp.com'],
-    content: {
-      type: 'html',
-      body: `🌟Completeness: 👎 <br><br><br>
-             🤖 Tone: 👎 <br><br><br>
-             💯Correctness: 👍 <br><br><br>`
-    },
-    replyOptions: {
-      type: 'reply',
-      originalMessageId: message.id
-    }
-  });
-}
-
-
-
-async function getMessage() {
-  console.log('Called getMessage()');
-  let list = await Front.listMessages();
-  console.log('List: ', list);
-  let messageId = list.results[0]['id'];
-  console.log(`returning message ID `, messageId);
-  return messageId;
+function loadCredentials(user) {
+  userCard = getContact(user.id)
+  console.log("User: ", userCard);
 }
