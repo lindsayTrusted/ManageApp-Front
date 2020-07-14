89cb2d82a7eb9dc5b7ca53f0d130cf2984bbcde3
lindsayTrusted <59939546+lindsayTrusted@users.noreply.github.com>
2020-07-14 00:31:57 -0700

    test11

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ Front.contextUpdates.subscribe(context => {
   var messageRecipient = document.getElementById('messageRecipient');
   messageRecipient.innerHTML = 'Displaying ' + context.conversation.recipient.contact.name;
 
-  console.log('Contact:', context.conversation.recipient.contact.customAttributes);
+  console.log('Contact:', context.conversation);
 
   assignButton.removeEventListener('click', _assign);
 
